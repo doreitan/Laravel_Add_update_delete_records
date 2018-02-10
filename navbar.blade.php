054db65391fce153a2b3fbbf50972ffd1f645bb1
Doron Eitan <32599090+doreitan@users.noreply.github.com>
2018-02-10 23:25:56 +0100

    Add files via upload

---
+++ navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="/">{{config("app.name", "Doro")}}</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">Home</a></li>
+            <li><a href="/about">about</a></li>
+            <li><a href="/services">services</a></li>
+            <li><a href="/contact">contact</a></li>
+            <li><a href="/posts">Blog</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+              <li> 
+                <a href= "/posts/create"> 
+                  Create Record 
+                </a>
+              </li>
+          </ul>
+
+        </div>
+      </nav>
