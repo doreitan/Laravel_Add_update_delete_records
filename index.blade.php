687ef446fbf86b263152e5d24fb7c76bfa598d0b
Doron Eitan <32599090+doreitan@users.noreply.github.com>
2018-02-10 23:27:11 +0100

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,18 @@
+@extends("layout.app")
+
+@section("content")
+<div class="jumbotron text-center">
+  <h1>Doro Doron Eitan</h1>
+  <p>www.doro.bandcamp.com</p>
+  <p>ברוכים הבאים לאתר הראשון שלי בלרבל</p>
+  <p><a class="btn btn-primary btn-lg" 
+    href="/login" role="button">Login</a>
+    <a class="btn btn-success btn-lg" 
+    href="/register" role="button">Register</a>
+  </p>
+</div>
+@endsection
+
+
+
+	 
